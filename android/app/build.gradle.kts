3c5e72c764ada25794c9b6015aa3de0acebd2c47
huojieren <1956934061@qq.com>
2025-08-27 17:25:10 +0800

    refactor: 重构项目结构并重命名应用为 Wan Android
    - 重命名包名和应用名
    - 更新图标和启动屏幕
    - 修改网页相关文件
    - 删除iOS端文件

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.huojieren.flutter_demo"
+    namespace = "com.huojieren.wan_android"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.huojieren.flutter_demo"
+        applicationId = "com.huojieren.wan_android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/huojieren/wan_android/MainActivity.kt
+++ android/app/src/main/kotlin/com/huojieren/wan_android/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.huojieren.flutter_demo
+package com.huojieren.wan_android
 
 import io.flutter.embedding.android.FlutterActivity
 
